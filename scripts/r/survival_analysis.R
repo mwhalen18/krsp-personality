149ed29f27c775a8eaf7f822cb2eb41811e966b9
Matthew Whalen <matthewwhalen@Matthews-MacBook-Pro.local>
2022-05-25 11:14:28 -0400

    raw analysis completed

--- scripts/r/survival_analysis.R
+++ scripts/r/survival_analysis.R
@@ -9,7 +9,7 @@ library(DHARMa)
   # Add 2021 litter data from krsp temp db
 
 
-personality = read_csv('mrw-scripts/data/personality-mrw-survival.csv', show_col_types = FALSE) %>% 
+personality = read_csv('data/personality-mrw-survival.csv', show_col_types = FALSE) %>% 
   mutate(survival = as.integer(survived_200d)) %>% 
   group_by(grid, year) %>% 
   mutate(growth_sc = scale(growth, scale = T, center = T)[,1],
@@ -34,19 +34,6 @@ oft1 = predict(pca_act)[,1]
 personality$oft1 = unlist(oft1)
 personality$mis1 = unlist(mis1)
 
-ggplot(personality, aes(oft1, mis1)) +
-  geom_point() +
-  stat_ellipse(level = 0.95) +
-  labs(x = "Activity", y = "Aggression") +
-  theme_minimal() +
-  theme(plot.margin = margin(20,20,20,20))
-
-
-
-ggplot(personality, aes(mis1, survival)) +
-  geom_point()
-
-
 # Survival to fall census -------------------------------------------------
 dat = personality %>% 
   mutate(across(c(year, dam_id, litter_id, grid), as_factor)) %>% 
@@ -67,18 +54,14 @@ survival_to_autumn = glmer(made_it ~ sex +
                            control = glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 1e8)))
 
 car::Anova(survival_to_autumn)
-
 summary(survival_to_autumn)
 
 # Diagnostics
 simulationOutput = simulateResiduals(survival_to_autumn, plot = F)
 
 residuals(simulationOutput, quantileFunction = qnorm)
-
 plot(simulationOutput)
 # Looks pretty good
-
-
 
 # Model 2 Survival to 200 days --------------------------------------------
 dat = personality %>% 
@@ -97,7 +80,6 @@ survival_to_200d = glmer(survival ~ sex +
                          na.action = 'na.omit',
                          family = 'binomial',
                          control = glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 1e8)))
-
 #allFit(mod)
 car::Anova(survival_to_200d)
 
@@ -105,7 +87,6 @@ car::Anova(survival_to_200d)
 simulationOutput = simulateResiduals(survival_to_200d, plot = F)
 
 residuals(simulationOutput, quantileFunction = qnorm)
-
 plot(simulationOutput)
 # Theres some wonkiness here with one of the random effects deviating
 # But it seems to not be a massive issue
